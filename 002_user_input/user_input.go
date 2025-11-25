6cb889c55c4f97853469f175205eba42b9d40791
yuri_rodnov <somewaystodo@yandex.ru>
2025-11-26 02:03:37 +0300

    add scan

--- 002_user_input/user_input.go
+++ 002_user_input/user_input.go
@@ -7,9 +7,14 @@ import (
 
 
 func main(){
-	var name string
-	fmt.Print("Type your name: ")
-	fmt.Scan(&name)
-	fmt.Println("Hello,", name)
+	// var name string
+	// fmt.Print("Type your name: ")
+	// fmt.Scan(&name)
+	// fmt.Println("Hello,", name)
+
+	var one, two, result int
+    fmt.Scan(&one, &two)
+    result = one + two
+    fmt.Println(result)
 
 }
